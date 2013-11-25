3ab8a3326d749bf70ab38e344a6245173b12f30a
Bayu ALdi Yansyah <bayualdiyansyah@gmail.com>
2013-11-25 21:55:56 +0700

    [grunt/todo#3] Add kickstarter code on Gruntfile.js
    Write some basic code for gruntjs configuration. divide inside wrapper
    function on 3 parts.
    
      1. Load plugin
      all code for load plugin goes here.
    
      2. Task configuration
      all code for configuration task inside `initConfig` object.
    
      3. Register custom tasl
      all code for register custom task goes here.
    
    TO DO:
      1. write grunt-contrib-connect configuration task.
      2. write grunt-recess configuration task.
      3. write grunt-contrib-watch configuration task.

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+// wrapper function
+// fungsi ini akan jadi selimut semua konfigurasi gruntjs
+module.exports = function (grunt) {
+
+  /** 
+   * load plugin
+   */
+  // tulis kode untuk load plugin disini
+
+  /** 
+   * konfigurasi task
+   */
+   grunt.initConfig({
+      // tulis kode untuk konfigurasi task disini
+   });
+
+  /** 
+   * register custom task
+   */
+  // tulis kode untuk register custom task disini
+}
